d63788ba20f3a5b26f4df688b488dace55f80a2a
Juan Castillo <castillojuan1000@gmail.com>
2020-07-04 13:09:49 -0400

    added the components needed

--- src/App.js
+++ src/App.js
@@ -3,13 +3,25 @@ import './App.css';
 import "tailwindcss/dist/base.css"
 import AnimationRevealPage from "helpers/AnimationRevealPage"
 import Hero from "components/hero/BackgroundAsImageWithCenteredContent"
+import About from "components/features/TwoColSingleFeatureWithStats2"
+import Staff from 'components/testimonials/ThreeColumnWithProfileImage'
+import Pilares from 'components/features/VerticalWithAlternateImageAndText'
+import FAQ from 'components/faqs/TwoColumnPrimaryBackground'
+import Contact from 'components/forms/TwoColContactUsWithIllustrationFullForm'
+import Footer from 'components/footers/MiniCenteredFooter'
 
 function App() {
   return (
     <AnimationRevealPage>
       <Hero />
-
+      <About />
+      <Staff />
+      <Pilares />
+      <FAQ />
+      <Contact />
+      <Footer />
     </AnimationRevealPage>
+
   )
 }
 
