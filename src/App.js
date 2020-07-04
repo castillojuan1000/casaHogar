d54d820d80731b53183fa40638d5c154120d870a
Juan Castillo <castillojuan1000@gmail.com>
2020-07-03 23:51:23 -0400

    casaHogar first commit, setup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react"
+import './App.css';
+import "tailwindcss/dist/base.css"
+import AnimationRevealPage from "helpers/AnimationRevealPage"
+import Hero from "components/hero/BackgroundAsImageWithCenteredContent"
+
+function App() {
+  return (
+    <AnimationRevealPage>
+      <Hero />
+
+    </AnimationRevealPage>
+  )
+}
+
+export default App
