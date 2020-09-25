37df5db5ed4798e436c06898401839d8d9b213ea
Juan Castillo <castillojuan1000@gmail.com>
2020-09-25 07:23:01 -0400

    Updates

---
+++ precache-manifest.c27a36b7a08b89414e070260a9523d80.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c73ba15c0b37466e0529c6d5c179ab40",
+    "url": "/casaHogar/index.html"
+  },
+  {
+    "revision": "6309c3e4a129bfdd2d47",
+    "url": "/casaHogar/static/css/2.6011fe8e.chunk.css"
+  },
+  {
+    "revision": "a863525dcfe3ae37332d",
+    "url": "/casaHogar/static/css/main.d9188528.chunk.css"
+  },
+  {
+    "revision": "6309c3e4a129bfdd2d47",
+    "url": "/casaHogar/static/js/2.9a526cae.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "/casaHogar/static/js/2.9a526cae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a863525dcfe3ae37332d",
+    "url": "/casaHogar/static/js/main.aed668e3.chunk.js"
+  },
+  {
+    "revision": "b86dafd80fbc97655bbe",
+    "url": "/casaHogar/static/js/runtime-main.c28e2440.js"
+  },
+  {
+    "revision": "f2bbc8e34e110c7f8f48bf3c852b1b02",
+    "url": "/casaHogar/static/media/MainBackground3.f2bbc8e3.png"
+  },
+  {
+    "revision": "dac043e496caea579de03dce251f9c8b",
+    "url": "/casaHogar/static/media/apostolado.dac043e4.png"
+  },
+  {
+    "revision": "319a321f4d259ea75f3a12ca091692c0",
+    "url": "/casaHogar/static/media/arrow-left-2-icon.319a321f.svg"
+  },
+  {
+    "revision": "41406a779fc77849c5e239173f4c4724",
+    "url": "/casaHogar/static/media/arrow-left-3-icon.41406a77.svg"
+  },
+  {
+    "revision": "42c238c46338189c711d0b6223cd220b",
+    "url": "/casaHogar/static/media/arrow-right-2-icon.42c238c4.svg"
+  },
+  {
+    "revision": "e5cd3c02a99df7a0698490d716530206",
+    "url": "/casaHogar/static/media/arrow-right-3-icon.e5cd3c02.svg"
+  },
+  {
+    "revision": "59e4a25afaf6e1938fa08ce4035973b7",
+    "url": "/casaHogar/static/media/chLogo.59e4a25a.png"
+  },
+  {
+    "revision": "c66b213d1dfff391c61146727032f068",
+    "url": "/casaHogar/static/media/chevron-down.c66b213d.svg"
+  },
+  {
+    "revision": "1c251c8ff2222207650b4f24a2159cbb",
+    "url": "/casaHogar/static/media/dot-pattern.1c251c8f.svg"
+  },
+  {
+    "revision": "a1ad24916d979e088c5e8c80b073f92b",
+    "url": "/casaHogar/static/media/email-illustration.a1ad2491.svg"
+  },
+  {
+    "revision": "5e030c93ee64514b9fcdc887289ce8a9",
+    "url": "/casaHogar/static/media/facebook-icon.5e030c93.svg"
+  },
+  {
+    "revision": "c1d4b2ad3f3b5886145b5f8c56dd2440",
+    "url": "/casaHogar/static/media/instagram.c1d4b2ad.svg"
+  },
+  {
+    "revision": "58383984fcf7cd1ac831eb8d9b9a86ca",
+    "url": "/casaHogar/static/media/love-illustration.58383984.svg"
+  },
+  {
+    "revision": "ca394ec8a4754ab9db293dcac626bdcb",
+    "url": "/casaHogar/static/media/menu.ca394ec8.svg"
+  },
+  {
+    "revision": "f0a2d06f1314a6ee08a9394931ced8c0",
+    "url": "/casaHogar/static/media/oracion.f0a2d06f.png"
+  },
+  {
+    "revision": "5d0ff9bc5818d6498df9d90437127f90",
+    "url": "/casaHogar/static/media/star-icon.5d0ff9bc.svg"
+  },
+  {
+    "revision": "06b7e9297b931c8f3c729d9666faf31f",
+    "url": "/casaHogar/static/media/svg-decorator-blob-4.06b7e929.svg"
+  },
+  {
+    "revision": "50a3868db49539d53742411308690eba",
+    "url": "/casaHogar/static/media/svg-decorator-blob-5.50a3868d.svg"
+  },
+  {
+    "revision": "7e80b9954c2ce5b1b47e99748d6482ed",
+    "url": "/casaHogar/static/media/trabajo.7e80b995.png"
+  },
+  {
+    "revision": "b0200bf7cbbb26c32989e7a0e4d19eef",
+    "url": "/casaHogar/static/media/x.b0200bf7.svg"
+  },
+  {
+    "revision": "45e500e6c6c6577b063b28adc39fb422",
+    "url": "/casaHogar/static/media/youtube-icon.45e500e6.svg"
+  }
+]);
